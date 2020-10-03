635e1e3b90575b93cfbeb3ed6f002faa527eb041
Dion Shalaku <ds42980@ubt-uni.net>
2020-10-03 13:00:29 +0200

    lidhja me database

---
+++ MoviesList/Dal/AppConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace MoviesList.Dal
+{
+    public class AppConfiguration
+    {
+        public static string ConnectionString
+        {
+            get
+            {
+                return ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+            }
+        }
+    }
+}
